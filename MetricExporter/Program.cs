bad245d6c684c2e7388433f6c18385160b94a33a
Gus Emery <nfemery@gmail.com>
2019-06-06 15:57:33 -0500

    Json working; configuration added

--- MetricExporter/Program.cs
+++ MetricExporter/Program.cs
@@ -1,4 +1,5 @@
-﻿using Newtonsoft.Json;
+﻿using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 
@@ -6,13 +7,18 @@ namespace MetricExporter
 {
     class Program
 
-
+    
     {
+        //static IConfiguration config;
         static void Main(string[] args)
         {
+            IConfiguration config = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+            .Build();
+            
             //var result = @"{ ""metricId"" : 20455618,""metricName"" : ""BTM|BTs|BT:605274|Component:19539|Average Response Time (ms)"",""metricPath"" : ""Business Transaction Performance|Business Transactions|SM-RemoteObjectServer|Device - Work Lookup|Average Response Time (ms)"",""frequency"" : ""SIXTY_MIN"",""metricValues"" : [ { ""startTimeInMillis"" : 1557000060000, ""occurrences"" : 711, ""current"" : 13, ""min"" : 0, ""max"" : 41796,""useRange"" : true, ""count"" : 51551936, ""sum"" : 314112437, ""value"" : 6, ""standardDeviation"" : 0 } ]}";
             var calls = new RestCalls();
-            var result = calls.getMetrics("a").Result;
+            var result = calls.getMetrics("controller/rest/applications/913/metric-data?metric-path=Business%20Transaction%20Performance%7CBusiness%20Transactions%7C*%7C%2Fajax%2Fdaily%7C*&time-range-type=BEFORE_NOW&duration-in-mins=10080").Result;
             //if (result.StartsWith('[') && result.EndsWith(']'))
             //    result = result.Substring(1, result.Length - 2);
             var x = JsonConvert.DeserializeObject<ICollection<MetricData>>(result);
--- MetricExporter/RestCalls.cs
+++ MetricExporter/RestCalls.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Configuration;
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
@@ -9,21 +11,34 @@ namespace MetricExporter
 {
     public class RestCalls
     {
+
         HttpClient client = new HttpClient();
+        private string baseAddress = string.Empty;
+        private string authKey = string.Empty;
         public RestCalls()
         {
-                    }
+            IConfiguration config = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+            .Build();
+            //authKey = ConfigurationManager.AppSettings["authKey"];
+            //baseAddress = ConfigurationManager.AppSettings["controllerURL"];
+            baseAddress = config["controllerURL"];
+            authKey = config["authKey"];
+            
+        }
 
         public async Task<string> getMetrics(string metricData)
         {
             MetricData product =new MetricData();
-            
-            client.BaseAddress = new Uri("https://honeywell-sps.saas.appdynamics.com/controller/rest/");
-            var path = "applications/BigLots-Prod/metric-data?metric-path=Business%20Transaction%20Performance%7CBusiness%20Transactions%7CSM-RemoteObjectServer%7CDevice%20-%20Work%20Lookup%7CAverage%20Response%20Time%20%28ms%29&time-range-type=BEFORE_NOW&duration-in-mins=43200&output=json";
+
+            client.BaseAddress = new Uri(baseAddress); //new Uri("https://honeywell-sps.saas.appdynamics.com/controller/rest/");
+
+            var path = metricData + "&output=json";  //"controller/rest/applications/BigLots-Prod/metric-data?metric-path=Business%20Transaction%20Performance%7CBusiness%20Transactions%7CSM-RemoteObjectServer%7CDevice%20-%20Work%20Lookup%7CAverage%20Response%20Time%20%28ms%29&time-range-type=BEFORE_NOW&duration-in-mins=43200&output=json";
             //HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, path);
 
             var byteArray = Encoding.ASCII.GetBytes("gus@honeywell-sps:appdynamics");
-            client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Basic", Convert.ToBase64String(byteArray));
+           
+            client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Basic", authKey);
             HttpResponseMessage response =  client.GetAsync(path).Result;
 
             if (response.IsSuccessStatusCode)
